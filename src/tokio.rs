cd4aabc74c5f8fcc3808125171c607e34aa48451
John Children <john.a.children@gmail.com>
2021-05-06 18:48:49 +0100

    PoC: Use tokio Handles to get current runtime

--- src/tokio.rs
+++ src/tokio.rs
@@ -1,7 +1,7 @@
 use std::{future::Future, thread};
 
 use ::tokio::{
-    runtime::{Builder, Runtime},
+    runtime::{Builder, Handle, Runtime},
     task,
 };
 use futures::future::pending;
@@ -30,7 +30,7 @@ pub use pyo3_asyncio_macros::tokio_main as main;
 #[cfg(all(feature = "attributes", feature = "testing"))]
 pub use pyo3_asyncio_macros::tokio_test as test;
 
-static TOKIO_RUNTIME: OnceCell<Runtime> = OnceCell::new();
+static TOKIO_RUNTIME_HANDLE: OnceCell<Handle> = OnceCell::new();
 
 const EXPECT_TOKIO_INIT: &str = "Tokio runtime must be initialized";
 
@@ -50,15 +50,15 @@ impl generic::Runtime for TokioRuntime {
     where
         F: Future<Output = ()> + Send + 'static,
     {
-        get_runtime().spawn(async move {
+        get_handle().spawn(async move {
             fut.await;
         })
     }
 }
 
 /// Initialize the Tokio Runtime with a custom build
-pub fn init(runtime: Runtime) {
-    TOKIO_RUNTIME
+pub fn init(runtime: Handle) {
+    TOKIO_RUNTIME_HANDLE
         .set(runtime)
         .expect("Tokio Runtime has already been initialized");
 }
@@ -72,7 +72,10 @@ fn current_thread() -> Runtime {
 
 fn start_current_thread() {
     thread::spawn(move || {
-        TOKIO_RUNTIME.get().unwrap().block_on(pending::<()>());
+        TOKIO_RUNTIME_HANDLE
+            .get()
+            .unwrap()
+            .block_on(pending::<()>());
     });
 }
 
@@ -82,13 +85,13 @@ fn start_current_thread() {
 /// This function will panic if called a second time. See [`init_current_thread_once`] if you want
 /// to avoid this panic.
 pub fn init_current_thread() {
-    init(current_thread());
+    init(current_thread().handle().clone());
     start_current_thread();
 }
 
 /// Get a reference to the current tokio runtime
-pub fn get_runtime<'a>() -> &'a Runtime {
-    TOKIO_RUNTIME.get().expect(EXPECT_TOKIO_INIT)
+pub fn get_handle<'a>() -> &'a Handle {
+    TOKIO_RUNTIME_HANDLE.get().expect(EXPECT_TOKIO_INIT)
 }
 
 fn multi_thread() -> Runtime {
@@ -104,7 +107,7 @@ fn multi_thread() -> Runtime {
 /// This function will panic if called a second time. See [`init_multi_thread_once`] if you want to
 /// avoid this panic.
 pub fn init_multi_thread() {
-    init(multi_thread());
+    init(multi_thread().handle().clone());
 }
 
 /// Ensure that the Tokio Runtime is initialized
@@ -112,7 +115,7 @@ pub fn init_multi_thread() {
 /// If the runtime has not been initialized already, the multi-thread scheduler
 /// is used. Calling this function a second time is a no-op.
 pub fn init_multi_thread_once() {
-    TOKIO_RUNTIME.get_or_init(|| multi_thread());
+    TOKIO_RUNTIME_HANDLE.get_or_init(|| multi_thread().handle().clone());
 }
 
 /// Ensure that the Tokio Runtime is initialized
@@ -121,9 +124,9 @@ pub fn init_multi_thread_once() {
 /// scheduler is used. Calling this function a second time is a no-op.
 pub fn init_current_thread_once() {
     let mut initialized = false;
-    TOKIO_RUNTIME.get_or_init(|| {
+    TOKIO_RUNTIME_HANDLE.get_or_init(|| {
         initialized = true;
-        current_thread()
+        current_thread().handle().clone()
     });
 
     if initialized {
@@ -148,27 +151,20 @@ pub fn init_current_thread_once() {
 /// # use std::time::Duration;
 /// #
 /// # use pyo3::prelude::*;
-/// # use tokio::runtime::{Builder, Runtime};
 /// #
-/// # let runtime = Builder::new_current_thread()
-/// #     .enable_all()
-/// #     .build()
-/// #     .expect("Couldn't build the runtime");
+/// # #[tokio::main]
+/// # async fn main() {
+/// #   pyo3_asyncio::tokio::init(tokio::runtime::Handle::current());
 /// #
-/// # Python::with_gil(|py| {
-/// # pyo3_asyncio::with_runtime(py, || {
-/// # pyo3_asyncio::tokio::init_current_thread();
-/// pyo3_asyncio::tokio::run_until_complete(py, async move {
-///     tokio::time::sleep(Duration::from_secs(1)).await;
-///     Ok(())
-/// })?;
-/// # Ok(())
-/// # })
-/// # .map_err(|e| {
-/// #    e.print_and_set_sys_last_vars(py);  
-/// # })
-/// # .unwrap();
-/// # });
+/// #   Python::with_gil(|py| {
+/// #       pyo3_asyncio::with_runtime(py, || {
+/// #           pyo3_asyncio::tokio::run_until_complete(py, async move {
+/// #             tokio::time::sleep(Duration::from_secs(1)).await;
+/// #             Ok(())
+/// #           })
+/// #       }) 
+/// #   }).unwrap() 
+/// # }
 /// ```
 pub fn run_until_complete<F>(py: Python, fut: F) -> PyResult<()>
 where
